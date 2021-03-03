7c5e94817ae05081ec7b06e1c83793499d730fdb
Alex Young <younga6@oregonstate.edu>
2021-03-03 01:13:40 -0800

    Implement Leap Year method, this uses modulo 4, 400 and 100 to check if a year is a leap year

---
+++ leap_year.py
@@ -0,0 +1,20 @@
+# CS362 HW 7
+# Alex Young
+# 3/2/2021
+# Run this file using python3 leap_year.py
+# This program finds if a year is a leap year
+
+def run(x):
+    try:
+        if (x % 4 == 0):
+            if (x % 400 == 0):
+                return "is a leap year"
+            else:
+                if (x % 100 == 0):
+                    return "is not a leap year"
+                else:
+                    return "is a leap year"
+        else:
+            return "is not a leap year"
+    except TypeError:
+        return("invalid input")
